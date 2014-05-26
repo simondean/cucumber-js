8c45380ce065bbd677e81ad73bcfe15954ab96a1
Simon Dean <simon@simondean.org>
2014-05-26 17:05:03 +0100

    Added unit tests for scenario.attach()

--- spec/cucumber/api/scenario_spec.js
+++ spec/cucumber/api/scenario_spec.js
@@ -2,7 +2,7 @@ require('../../support/spec_helper');
 
 describe("Cucumber.Api.Scenario", function() {
   var Cucumber = requireLib('cucumber');
-  var scenarioFailed, scenarioPending, scenarioSuccessful, scenarioUndefined, astTreeWalker;
+  var scenarioFailed, scenarioPending, scenarioSuccessful, scenarioUndefined, attachments, astTreeWalker;
   var keyword, name, description, uri, line, tags, astScenario;
   var scenario;
 
@@ -12,7 +12,7 @@ describe("Cucumber.Api.Scenario", function() {
     scenarioSuccessful = createSpy("scenario successful");
     scenarioUndefined  = createSpy("scenario undefined");
     attachments        = createSpy("attachments");
-    astTreeWalker      = createSpyWithStubs("ast scenario", { isScenarioFailed: scenarioFailed, isScenarioPending: scenarioPending, isScenarioSuccessful: scenarioSuccessful, isScenarioUndefined: scenarioUndefined, getAttachments: attachments });
+    astTreeWalker      = createSpyWithStubs("ast scenario", { isScenarioFailed: scenarioFailed, isScenarioPending: scenarioPending, isScenarioSuccessful: scenarioSuccessful, isScenarioUndefined: scenarioUndefined, getAttachments: attachments, attach: undefined });
     keyword            = createSpy("scenario keyword");
     name               = createSpy("scenario name");
     description        = createSpy("scenario description");
@@ -87,6 +87,135 @@ describe("Cucumber.Api.Scenario", function() {
   describe("getAttachments()", function() {
     it("returns any attachments created by the current step", function() {
       expect(scenario.getAttachments()).toBe(attachments);
+    });
+  });
+
+  describe("attach()", function() {
+    var mimeType, callback;
+
+    beforeEach(function() {
+      mimeType = createSpy("mime type");
+      callback = createSpy("callback");
+    });
+
+    describe("when the data is a stream.Readable", function() {
+      var stream;
+
+      beforeEach(function() {
+        stream = {pipe: function() {}};
+      });
+
+      it("throws an exception when the mimeType argument is missing", function() {
+        expect(function() { scenario.attach(stream); }).toThrow(new Error("Cucumber.Api.Scenario.attach() expects a mimeType"));
+      });
+
+      it("throws an exception when the callback argument is missing", function() {
+        expect(function() { scenario.attach(stream, mimeType); }).toThrow(new Error("Cucumber.Api.Scenario.attach() expects a callback when data is a stream.Readable"));
+      });
+
+      describe("when it reads the stream", function() {
+        var dataListener, endListener;
+
+        beforeEach(function() {
+          spyOnStub(stream, "on").andCallFake(function(event, listener) {
+            if (event === "data") {
+              dataListener = listener;
+            }
+            else if (event === "end") {
+              endListener = listener;
+            }
+            else {
+              throw new Error("Unrecognised event " + event);
+            }
+          });
+          spyOnStub(astTreeWalker, "attach");
+
+          scenario.attach(stream, mimeType, callback);
+        })
+
+        it("does not call back straight away", function() {
+          expect(callback).not.toHaveBeenCalled();
+        });
+
+        it("listens for the data event on the stream", function() {
+          expect(dataListener).toBeAFunction();
+        });
+
+        it("listens for the end event on the stream", function() {
+          expect(endListener).toBeAFunction();
+        });
+
+        describe("when the stream finishes providing data", function() {
+          beforeEach(function() {
+            dataListener(new Buffer("first chunk"));
+            dataListener(new Buffer("second chunk"));
+            endListener();
+          });
+
+          it("instructs the ast tree walker to create an attachment containing the base 64 encoded contents of the stream", function() {
+            expect(astTreeWalker.attach).toHaveBeenCalledWith("Zmlyc3QgY2h1bmtzZWNvbmQgY2h1bms=", mimeType);
+          });
+
+          it("calls back", function() {
+            expect(callback).toHaveBeenCalled();
+          });
+        });
+      })
+    });
+
+    describe("when the data is a Buffer", function() {
+      var buffer;
+
+      beforeEach(function() {
+        buffer = new Buffer("data");
+      })
+
+      it("throws an exception when the mimeType argument is missing", function() {
+        expect(function() { scenario.attach(buffer); }).toThrow(new Error("Cucumber.Api.Scenario.attach() expects a mimeType"));
+      });
+
+      it("instructs the ast tree walker to create an attachment containing the base 64 encoded contents of the buffer", function() {
+        scenario.attach(buffer, mimeType);
+        expect(astTreeWalker.attach).toHaveBeenCalledWith("ZGF0YQ==", mimeType);
+      });
+
+      describe("when provided with a callback", function() {
+        beforeEach(function() {
+          scenario.attach(buffer, mimeType, callback);
+        });
+
+        it("calls back", function() {
+          expect(callback).toHaveBeenCalled();
+        });
+      });
+
+      describe("when not provided with a callback", function() {
+        beforeEach(function() {
+          scenario.attach(buffer, mimeType);
+        });
+
+        it("does not call back", function() {
+          expect(callback).not.toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe("when the data is a string", function() {
+      var data;
+
+      beforeEach(function() {
+        data = "data";
+      })
+
+      it("instructs the ast tree walker to create an attachment containing the string", function() {
+        scenario.attach(data, mimeType);
+        expect(astTreeWalker.attach).toHaveBeenCalledWith(data, mimeType);
+      });
+
+      it("defaults to the plain text mime type when the mimeType argument is missing", function() {
+        scenario.attach(data);
+        expect(astTreeWalker.attach).toHaveBeenCalledWith(data, "text/plain");
+      });
     });
   });
 });
